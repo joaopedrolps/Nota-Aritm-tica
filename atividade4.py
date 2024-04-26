95c7d629fe099fa37a2df6acb4dbb47b37b65059
João Pedro <125750923+joaopedrolps@users.noreply.github.com>
2024-04-25 21:33:36 -0300

    Update atividade4.py

--- atividade4.py
+++ atividade4.py
@@ -1,4 +1,3 @@
-#1 - Faça um programa em Python que receba o nome e as quatro notas de um aluno, calcule e mostre a média aritmética. No final mostre a mensagem de Aprovado se a média for maior ou igual a 6 ou Reprovado se a média for menor que 6.
 nome2 = input("Qual o seu nome? ")
 
 
@@ -15,12 +14,3 @@ if conta1 >= 6:
     print("Sua situação é: Aprovado")
 
 else: print("Sua situação é: Reprovado")
-
-#2 - Faça um programa em Python que receba um número inteiro e imprima uma mensagem mostrando se o número digitado é par ou ímpar.
-
-numero = int(input('Digite um inteiro: '))
-
-if (numero%2) == 0:
-        print("O número digitado é par")
-else:
-        print("O número digitado é ímpar")
